59901a4af43cb78b72bb1a939206570d59052968
kaya <kayaskov@hotmail.com>
2018-12-07 17:32:48 +0100

    Html verbeterd + css in huisstijl begin

---
+++ script.js
@@ -0,0 +1,7 @@
+/*jslint browser: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+var form = document.getElementById("form");
+var zoeken_knop = document.getElementById("zoeken_knop");
+
